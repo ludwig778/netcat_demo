50445e429206940351627c776b6a8e7f258ceabd
ludwig778 <laurent.arthur75@gmail.com>
2017-01-17 15:01:28 +0100

    Update http_loop.sh

--- http_loop.sh
+++ http_loop.sh
@@ -2,5 +2,5 @@
 
 while :;
 do
-    echo -e "HTTP/1.1 200 OK\n\n<h1>Welcome to the netcat demo site6</h1><h3>Hosted by "$(hostname)"</h3>" | nc -l -p 8090;
+    echo -e "HTTP/1.1 200 OK\n\n<h1>Welcome to the netcat demo site7</h1><h3>Hosted by "$(hostname)"</h3>" | nc -l -p 8090;
 done
